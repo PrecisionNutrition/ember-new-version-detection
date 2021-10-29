fadd2e2b65dd9229cfea6a88280ca2a40ee400e3
James Herdman <james.herdman@gmail.com>
2021-10-29 14:31:25 -0400

    [ST-2087] Type JsonApi Adapter

--- addon/adapter/json-api.ts
+++ addon/adapter/json-api.ts
@@ -2,10 +2,11 @@ import JSONAPIAdapter from '@ember-data/adapter/json-api';
 import { next as runNext } from '@ember/runloop';
 import { inject as service } from '@ember/service';
 import fetchHeader from '../utils/fetch-header';
+import type NewVersionDetector from 'ember-new-version-detection/services/new-version-detector';
 
 export default class VersionDetectingJsonApiAdapter extends JSONAPIAdapter {
   @service('new-version-detector')
-  newVersionDetector;
+  declare newVersionDetector: NewVersionDetector;
 
   get headers() {
     return {
@@ -14,11 +15,13 @@ export default class VersionDetectingJsonApiAdapter extends JSONAPIAdapter {
     };
   }
 
-  handleResponse(_, headers) {
+  // eslint-disable-next-line @typescript-eslint/ban-types
+  handleResponse(status: number, headers: Record<string, unknown>, payload: {}, requestData: {}) {
     runNext(this, function () {
-      this.newVersionDetector.activeVersion = fetchHeader('X-Current-Version', headers);
+      const activeVersion = fetchHeader('X-Current-Version', headers);
+      this.newVersionDetector.activeVersion = activeVersion;
     });
 
-    return super.handleResponse(...arguments);
+    return super.handleResponse(status, headers, payload, requestData);
   }
 }
--- addon/utils/fetch-header.ts
+++ addon/utils/fetch-header.ts
@@ -1,3 +1,8 @@
-export default function fetchHeader(header: string, headers: Record<string, unknown>): unknown {
-  return headers[header] || headers[header.toLowerCase()];
+export default function fetchHeader(
+  header: string,
+  headers: Record<string, unknown>
+): string | null {
+  const value = headers[header] || headers[header.toLowerCase()];
+
+  return typeof value === 'string' ? value : null;
 }
