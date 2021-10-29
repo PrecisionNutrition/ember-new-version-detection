c407b39ce9d22ee542659ab883ba0007b52bc8d0
James Herdman <james.herdman@gmail.com>
2021-10-29 14:22:48 -0400

    [ST-2087] Type NewVersionDetector

--- addon/services/new-version-detector.ts
+++ addon/services/new-version-detector.ts
@@ -1,38 +1,54 @@
 import Service from '@ember/service';
+import { assert } from '@ember/debug';
 import { getOwner } from '@ember/application';
 import { tracked } from '@glimmer/tracking';
+import type ApplicationInstance from '@ember/application/instance';
+
+interface PartialAppConfig {
+  APP: {
+    _appVersion?: string;
+  };
+  'ember-new-version-detection'?: {
+    appName: string;
+  };
+}
 
 export default class NewVersionDetector extends Service {
-  get _appConfig() {
-    const config = getOwner(this).resolveRegistration('config:environment');
-    return config;
+  get _appConfig(): PartialAppConfig {
+    const owner = getOwner(this) as ApplicationInstance;
+    return owner.resolveRegistration('config:environment') as PartialAppConfig;
   }
 
   // Version as reported by the app build
-  get _rawVersion() {
+  get _rawVersion(): string | undefined {
     return this._appConfig.APP._appVersion;
   }
 
   /**
    * Configured name of the host application
    */
-  get appName() {
-    return this._appConfig['ember-new-version-detection'].appName;
+  get appName(): string {
+    const addonConfig = this._appConfig['ember-new-version-detection'];
+    const appName = addonConfig?.appName;
+
+    assert('You must set the application name. See README.md', typeof appName === 'string');
+
+    return appName;
   }
 
   // activeVersion (optional): string -- the version currently active, as reported by the API
   @tracked
-  activeVersion = null;
+  activeVersion: string | null = null;
 
   // ignoredVersions (optional): string -- version the user doesn't care about, perhaps they just want to get on with their life
   @tracked
-  ignoredVersion = null;
+  ignoredVersion: string | null = null;
 
-  get reportedVersion() {
+  get reportedVersion(): string | undefined {
     return this._rawVersion?.slice(6);
   }
 
-  get currentVersion() {
+  get currentVersion(): string | null {
     const rawVersion = this._rawVersion;
 
     if (rawVersion) {
@@ -45,7 +61,7 @@ export default class NewVersionDetector extends Service {
     }
   }
 
-  get isUpgradeAvailable() {
+  get isUpgradeAvailable(): boolean {
     const { activeVersion, currentVersion, ignoredVersion } = this;
 
     return !!(
@@ -59,7 +75,14 @@ export default class NewVersionDetector extends Service {
   /**
    * Ignores the current upgrade. Subsequent upgrades will not be ignored.
    */
-  ignoreThisUpgrade() {
+  ignoreThisUpgrade(): void {
     this.ignoredVersion = this.activeVersion;
+  }
+}
+
+// DO NOT DELETE: this is how TypeScript knows how to look up your services.
+declare module '@ember/service' {
+  interface Registry {
+    'new-version-detector': NewVersionDetector;
   }
 }
